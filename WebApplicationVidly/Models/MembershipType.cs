c227d11bc5b985dd6b77eac8add1e0b061dc2201
Touhidur Rahaman <touhidur.rahaman.antu@gmail.com>
2019-01-13 21:51:26 +0600

    Adding membership type name

--- WebApplicationVidly/Models/MembershipType.cs
+++ WebApplicationVidly/Models/MembershipType.cs
@@ -11,5 +11,6 @@ namespace WebApplicationVidly.Models
         public short SignUpFee { get; set; }
         public byte DurationInMonth { get; set; }
         public byte DiscountRate { get; set; }
+        public string Name { get; set; }
     }
 }
